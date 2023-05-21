82c8eed18dc3b7f0b77cbc98e169686e75abcfda
ifMaxi <maxi96p@gmail.com>
2023-05-21 17:12:49 -0300

    Font added

--- app/src/main/java/com/mp/todocompose/ui/theme/Type.kt
+++ app/src/main/java/com/mp/todocompose/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.mp.todocompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mp.todocompose.R
+
+val montserratFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
